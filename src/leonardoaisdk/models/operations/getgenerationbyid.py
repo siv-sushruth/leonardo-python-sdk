183e99eb18c73d273b5184091c6a7d600b263e27
siv-sushruth <151988829+siv-sushruth@users.noreply.github.com>
2024-07-31 14:28:50 +0530

    Update getgenerationbyid.py

--- src/leonardoaisdk/models/operations/getgenerationbyid.py
+++ src/leonardoaisdk/models/operations/getgenerationbyid.py
@@ -41,21 +41,11 @@ class GetGenerationByIDGeneratedImageVariationGeneric:
 class GeneratedImages:
     r"""columns and relationships of \\"generated_images\\" """
     UNSET='__SPEAKEASY_UNSET__'
-    fantasy_avatar: Optional[bool] = dataclasses.field(default=UNSET, metadata={'dataclasses_json': { 'letter_case': utils.get_field_name('fantasyAvatar'), 'exclude': lambda f: f is GeneratedImages.UNSET }})
-    r"""If fantasyAvatar feature was used."""
     generated_image_variation_generics: Optional[List[GetGenerationByIDGeneratedImageVariationGeneric]] = dataclasses.field(default=None, metadata={'dataclasses_json': { 'letter_case': utils.get_field_name('generated_image_variation_generics'), 'exclude': lambda f: f is None }})
     id: Optional[str] = dataclasses.field(default=UNSET, metadata={'dataclasses_json': { 'letter_case': utils.get_field_name('id'), 'exclude': lambda f: f is GeneratedImages.UNSET }})
-    image_to_video: Optional[bool] = dataclasses.field(default=UNSET, metadata={'dataclasses_json': { 'letter_case': utils.get_field_name('imageToVideo'), 'exclude': lambda f: f is GeneratedImages.UNSET }})
-    r"""If it is an image to video generation."""
     like_count: Optional[int] = dataclasses.field(default=None, metadata={'dataclasses_json': { 'letter_case': utils.get_field_name('likeCount'), 'exclude': lambda f: f is None }})
-    motion: Optional[bool] = dataclasses.field(default=UNSET, metadata={'dataclasses_json': { 'letter_case': utils.get_field_name('motion'), 'exclude': lambda f: f is GeneratedImages.UNSET }})
-    r"""If generation is of motion type."""
     motion_mp4_url: Optional[str] = dataclasses.field(default=UNSET, metadata={'dataclasses_json': { 'letter_case': utils.get_field_name('motionMP4URL'), 'exclude': lambda f: f is GeneratedImages.UNSET }})
     r"""The URL of the motion MP4."""
-    motion_model: Optional[str] = dataclasses.field(default=UNSET, metadata={'dataclasses_json': { 'letter_case': utils.get_field_name('motionModel'), 'exclude': lambda f: f is GeneratedImages.UNSET }})
-    r"""The name of the motion model."""
-    motion_strength: Optional[int] = dataclasses.field(default=UNSET, metadata={'dataclasses_json': { 'letter_case': utils.get_field_name('motionStrength'), 'exclude': lambda f: f is GeneratedImages.UNSET }})
-    r"""The motion strength."""
     nsfw: Optional[bool] = dataclasses.field(default=None, metadata={'dataclasses_json': { 'letter_case': utils.get_field_name('nsfw'), 'exclude': lambda f: f is None }})
     url: Optional[str] = dataclasses.field(default=None, metadata={'dataclasses_json': { 'letter_case': utils.get_field_name('url'), 'exclude': lambda f: f is None }})
     
